dda37a6be142cb6fd3e097995b13c2e960f74681
ktn001 <>
2022-02-03 10:04:34 +0100

    premier jet avec un contenu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
- 
+
+$(function(){
+	$('.sidenav').sidenav();
+	setTimeout(function(){
+    if ($('#slide-out').length > 0 && window.matchMedia("only screen and (max-width: 760px)").matches) {
+      $('.sidenav').sidenav();
+    }
+  }, 100);
+  setTimeout(function(){
+    if ($('#slide-out').length > 0 && window.matchMedia("only screen and (max-width: 760px)").matches) {
+      $('.sidenav').sidenav();
+    }
+  }, 250);
+  setTimeout(function(){
+    if ($('#slide-out').length > 0 && window.matchMedia("only screen and (max-width: 760px)").matches) {
+      $('.sidenav').sidenav();
+    }
+  }, 500);
+	$('.collapsible').collapsible();
+	$('.dropdown-trigger').dropdown();
+})
+
